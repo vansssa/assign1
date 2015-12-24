7bde6a87cd775075151139f7270de583ab0d89fc
vansssa <sqa-pt@KKBOXdeMacBook-Air-80.local>
2015-12-24 09:57:50 +0800

    調整格式

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,11 @@
 
 def is_perfect(num):
-	return sum([ i for i in range(1, num) if num%i == 0 ]) == num 
-	#result=0
-	#for i in listnumber:
-	#	if num%i==0:
-	#		result=i+result
-	#return result==num
+    return sum([i for i in range(1, num) if num % i == 0]) == num
+    # result=0
+    # for i in listnumber:
+    #	if num%i==0:
+    #		result=i+result
+    # return result==num
 
 print is_perfect(6)
 print is_perfect(8)
-
